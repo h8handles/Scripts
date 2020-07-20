a43020e4bef760e7866e8abcf5bb1199839183d6
h8handles <67421443+h8handles@users.noreply.github.com>
2020-07-20 18:41:05 -0400

    Update Pin_list_narrow.sh

--- Pin_list_narrow.sh
+++ Pin_list_narrow.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-
+#quick dirty way to remove the characters on the most common pin list in the SecList 
+#removes characters after the ":" must change to a .txt file!
 
 while IFS= read -r line; do
     echo "$line" | sed 's/,.*//' pins.txt
