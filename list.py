75ffbeff9f342b85a7910912805fde276baab22e
h8handles <67421443+h8handles@users.noreply.github.com>
2022-09-08 18:06:22 -0400

    LOTR  name scraper
    You can scrape any site to generate list of users just change url and the html tag you are looking for

---
+++ list.py
@@ -0,0 +1,32 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+
+r = requests.get("https://www.giantbomb.com/the-lord-of-the-rings/3025-481/characters/?page=2")
+
+
+#print(r.status_code)
+
+
+soup = BeautifulSoup(r.text,"html.parser")
+
+
+headers = soup.findAll('h3','title')
+
+
+for items in headers:
+	names = items.find(text=True)
+	names = str(names)
+	nospace=names.replace(" ","")
+	#print(nospace)
+	new = re.sub(r'\'\w+', '', nospace)
+	#the below print statement confirms names is of type string
+	#print(type(names))
+	print(new)
+
+
+
+
+
+#print(soup)
